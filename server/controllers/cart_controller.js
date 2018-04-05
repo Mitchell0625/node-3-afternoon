fa1e246388f94cd46b37e7b262090836326a84f5
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-04-04 19:34:40 -0500

    corrected delete method

--- server/controllers/cart_controller.js
+++ server/controllers/cart_controller.js
@@ -18,21 +18,32 @@ module.exports = {
     res.status(200).json(req.session.user);
   },
   delete: (req, res, next) => {
+    //Not sure why my commented code does not work
+
+    // const { id } = req.query;
+    // let { cart } = req.session.user;
+    // const selectSwag = swag.find(swag.id == id);
+    // console.log(req.session);
+    // if (selectSwag) {
+    //   const index = cart.findIndex(swag => {
+    //     swag.id == id;
+    //   });
+    //   cart.splice(index, 1);
+    //   req.session.user.total -= selectSwag.price;
+    // }
+    // res.status(200).json(req.session.user);
+
     const { id } = req.query;
-    let { cart } = req.session.user;
+    const { cart } = req.session.user;
 
-    const selectSwag = swag.find(swag.id == id);
-    console.log(req.session);
-    if (selectSwag) {
-      const index = cart.findIndex(swag => {
-        swag.id == id;
-      });
+    const selectedSwag = swag.find(swag => swag.id == id);
 
-      cart.splice(index, 1);
-      req.session.user.total -= selectSwag.price;
+    if (selectedSwag) {
+      const i = cart.findIndex(swag => swag.id == id);
+      cart.splice(i, 1);
+      req.session.user.total -= selectedSwag.price;
     }
-
-    res.status(200).json(req.session.user);
+    res.status(200).send(req.session.user);
   },
 
   checkout: (req, res, next) => {
